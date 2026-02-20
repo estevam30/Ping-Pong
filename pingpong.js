d455265fd7e04030969e84f283e6a76b735dd558
estevam30 <estevam.diniz@escola.pr.gov.br>
2026-02-20 07:47:53 -0300

    Create pingpong.js

---
+++ pingpong.js
@@ -0,0 +1,29 @@
+// Criar Bolinha
+let raio = 7.5;
+let xBolinha = 100;
+let yBolinha = 200;
+let diamante = raio * 2;
+
+// Velocidade deea bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+// Minha Raquete
+let xRaquete = 5;
+let YRaquete = 150;
+
+// Raquete Oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+
+//Placar
+let meusPontos = 0;
+let pontosOponentes = 0;
+
+function setup(){
+  createCanvas(600,400)
+}
+
+function draw (){
+  
+}
